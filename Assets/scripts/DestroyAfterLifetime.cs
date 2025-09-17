531312482919488c5b7562ddabf9a8e00f808ce6
JonnaHolmgren <jonna.holmgren.hem@gmail.com>
2025-09-17 12:53:39 +0200

    Skrev Hej

--- Assets/scripts/DestroyAfterLifetime.cs
+++ Assets/scripts/DestroyAfterLifetime.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Hej
+
 public class DestroyAfterLifetime : MonoBehaviour
 {
     [SerializeField] private float lifeTime = 1.0f;
